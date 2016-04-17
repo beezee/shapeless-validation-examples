485f0db032bcc334d3f0fedd0a73ec6f7549daa4
beezee <brian.zeligson@gmail.com>
2016-04-17 17:17:16 -0400

    clean up imports, make left side generic

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,6 +1,6 @@
-import shapeless._
+import shapeless.{HList, HNil}
 import shapeless.labelled.{field, FieldType}
-import shapeless.Lazy
+import shapeless.{LabelledGeneric, Lazy, Witness}
 import shapeless.ops.hlist.Mapper
 import shapeless.ops.record.{Fields, Keys}
 import shapeless.syntax.singleton._
@@ -10,50 +10,49 @@ import scalaz.syntax.std.string._
 import scalaz.syntax.validation._
 import scalaz.{\/, NonEmptyList, ValidationNel}
 
-trait Parser[A] {
-  def apply(ma: Map[String, Any]): ValidationNel[String, A]
+trait Parser[L, A] {
+  def apply(ma: Map[String, Any]): ValidationNel[L, A]
 }
 
-trait FieldParser[A] {
-  def apply(k: String, ma: Map[String, Any]): ValidationNel[String, A]
+trait FieldParser[L, A] {
+  def apply(k: String, ma: Map[String, Any]): ValidationNel[L, A]
 }
-
-object toName extends Poly1 { implicit def keys[A] = at[Symbol with A](_.name) }
 
 object Parser {
-  implicit val hnilParser: Parser[HNil] = new Parser[HNil] {
-    def apply(ma: Map[String, Any]): ValidationNel[String, HNil] =
-      HNil.successNel[String]
+  implicit def hnilParser[L]: Parser[L, HNil] = new Parser[L, HNil] {
+    def apply(ma: Map[String, Any]): ValidationNel[L, HNil] =
+      HNil.successNel[L]
   }
 
-  implicit def hconsParser[K <: Symbol, H: FieldParser, T <: HList: Parser](implicit key: Witness.Aux[K]):
-  Parser[FieldType[K, H] :: T] = new Parser[FieldType[K, H] :: T] {
-    def apply(ma: Map[String, Any]): ValidationNel[String, FieldType[K, H] :: T] =
-        (implicitly[FieldParser[H]].apply(key.value.name, ma).map(field[K](_))
-          |@| implicitly[Parser[T]].apply(ma))(_ :: _)
+  implicit def hconsParser[L, K <: Symbol, H, T <: HList](
+    implicit key: Witness.Aux[K], fp: FieldParser[L, H], tp: Parser[L, T]):
+  Parser[L, shapeless.::[FieldType[K, H], T]] = new Parser[L, shapeless.::[FieldType[K, H], T]] {
+    def apply(ma: Map[String, Any]): ValidationNel[L, shapeless.::[FieldType[K, H], T]] =
+        (fp.apply(key.value.name, ma).map(field[K](_))
+          |@| implicitly[Parser[L, T]].apply(ma))(_ :: _)
   }
 
-  implicit val stringParser: FieldParser[String] = new FieldParser[String] {
+  implicit val stringParser: FieldParser[String, String] = new FieldParser[String, String] {
     def apply(k: String, ma: Map[String, Any]): ValidationNel[String, String] =
       \/.fromTryCatchNonFatal(ma(k).asInstanceOf[String])
         .leftMap(_ => s"$k not a string".wrapNel)
         .validation
   }
 
-  implicit val intParser: FieldParser[Int] = new FieldParser[Int] {
+  implicit val intParser: FieldParser[String, Int] = new FieldParser[String, Int] {
     def apply(k: String, ma: Map[String, Any]): ValidationNel[String, Int] =
       \/.fromTryCatchNonFatal(ma(k).asInstanceOf[Int])
         .leftMap(_ => s"$k not an int".wrapNel)
         .validation
   }
 
-  def apply[A](ma: Map[String, Any])(implicit parser: Parser[A]): ValidationNel[String, A] = parser(ma)
+  def apply[L, A](ma: Map[String, Any])(implicit parser: Parser[L, A]): ValidationNel[L, A] = parser(ma)
 
-  implicit def caseClassParser[A, R <: HList](implicit
+  implicit def caseClassParser[L, A, R <: HList](implicit
     gen: LabelledGeneric.Aux[A, R],
-    reprParser: Lazy[Parser[R]]
-  ): Parser[A] = new Parser[A] {
-    def apply(ma: Map[String, Any]): ValidationNel[String, A] = reprParser.value.apply(ma).map(gen.from)
+    reprParser: Lazy[Parser[L, R]]
+  ): Parser[L, A] = new Parser[L, A] {
+    def apply(ma: Map[String, Any]): ValidationNel[L, A] = reprParser.value.apply(ma).map(gen.from)
   }
 
 }
